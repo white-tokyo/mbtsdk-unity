7ecf516bad0d477bb2a663adaa20944d241d1732
moajo <mimirosiasd@gmail.com>
2016-05-17 20:31:12 +0900

    refactor MBTHelper

--- scripts/MBTHelper.cs
+++ scripts/MBTHelper.cs
@@ -7,35 +7,30 @@ public class MBTHelper : MonoBehaviour {
 	public delegate void TouchPad(float x, float y);
 	public delegate void Gesture(string name);
 
+	//タップの許容時間
 	public const float tapDetectTime = 1.0f;
 
+	//タップの許容移動距離
 	public const float tapDetectLength = 15.0f;
 	private const float tapDetectLengthSqrt = tapDetectLength*tapDetectLength;
 
-	public float doubleTapDetectTime = 0.15f;
+	//ダブルタップの許容時間
+	public float doubleTapDetectTime = 0.3f;
 
+	//チャージに必要なフレーム数
 	public int chargeDetectTapCount = 30;
 
-	private Vector3 currentPos = Vector3.zero;
-	private bool isScroll = false;
-	private Vector3 tapPos = Vector3.zero;
-	private float tapTime = 0.0f;
+	private Vector2 tapBeganPos = Vector2.zero;
+
+	//タッチ継続時間
+	private float touchTime = 0.0f;
 	private float lastTapTime = 0.0f;
 	private int tapCountPerTime = 0;
 	private int lastTime = 0;
-	private bool isTap = false;
 	private bool charged = false;
 
 	public float minSwipeDistX;
 	public float minSwipeDistY;
-
-	private float swipeDistX;
-	private float swipeDistY;
-
-	private float SignValueX;
-	private float SignValueY;
-
-	private Vector2 endPos;
 
 	void Start () {
 		if (minSwipeDistX == 0) {
@@ -64,12 +59,16 @@ public class MBTHelper : MonoBehaviour {
 
 	void Update () {
 
-		if (Input.touchCount > 0) {
+		if (Input.touchCount == 0) {
+			return;
+		}
 
+		//check charge
+		if(!charged){
 			if (lastTime == (int)Time.time) {
 				tapCountPerTime += 1;
 
-				if (tapCountPerTime > chargeDetectTapCount && !charged) {
+				if (tapCountPerTime > chargeDetectTapCount) {
 					charged = true;
 					OnCharge ();
 				}
@@ -77,85 +76,64 @@ public class MBTHelper : MonoBehaviour {
 				tapCountPerTime = 0;
 				lastTime = (int)Time.time;
 			}
-
-			if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
-				isTap = true;
-
-				if ((Time.time - lastTapTime) < doubleTapDetectTime) {
-					OnDoubleTap ();
-				} else {
-					lastTapTime = Time.time;
-				}
-
-				isScroll = true;
-				tapPos = Input.GetTouch(0).position;
-				currentPos = Input.GetTouch(0).position;
-			}
-			switch (Input.GetTouch(0).phase) {
-			case TouchPhase.Began:
-			case TouchPhase.Moved:
-			case TouchPhase.Stationary:
-				tapTime += Time.deltaTime;
-
-				Vector3 vv = Input.GetTouch(0).position;
-				Vector3 vp = currentPos - vv;
-				if (isScroll && vp.sqrMagnitude > tapDetectLengthSqrt) {
-					if (vp.x*vp.x > vp.y*vp.y){
-						OnScroll(Input.GetTouch(0).position.x, 0);
-						isTap = false;
-					} else {
-						OnScroll(0, Input.mousePosition.y);
-						isTap = false;
-					}
-				}
-				currentPos = vv;
-				break;
-
-			case TouchPhase.Ended:
-
-				endPos = new Vector2 (tapPos.x, tapPos.y);
-
-				swipeDistX = (new Vector3 (endPos.x, 0, 0) - new Vector3 (currentPos.x, 0, 0)).magnitude;
-				if (swipeDistX > minSwipeDistX) {
-					SignValueX = Mathf.Sign (endPos.x - currentPos.x);
-
-					if (SignValueX > 0) {
-						OnSwipe ("RIGHTSwipe");
-					} else if (SignValueX < 0) {
-						OnSwipe ("LEFTSwipe");
-					}
-				}
-
-				swipeDistY = (new Vector3 (0, endPos.y, 0) - new Vector3 (0, currentPos.y, 0)).magnitude;
-				if (swipeDistY > minSwipeDistY) {
-					SignValueY = Mathf.Sign (endPos.y - currentPos.y);
-
-					if (SignValueY > 0) {
-						OnSwipe ("UPSwipe");
-					} else if (SignValueY < 0) {
-						OnSwipe ("DownSwipe");
-					}
-				}
-
-				if (isScroll) {
-					vp = currentPos - tapPos;
-					// タップした時間がタップ判定時間より短かった場合
-					if (tapTime < tapDetectTime) {
-						if (vp.sqrMagnitude < tapDetectLengthSqrt && isTap) {
-							OnTap ();
-						}
-					} else {
-						if (vp.x * vp.x < vp.y * vp.y) {
-							OnScroll (0, Input.GetTouch (0).position.y);
-						}
-					}
-				}
-
-				isTap = false;
-				isScroll = false;
-				tapTime = 0.0f;
-				break;
-			}
 		}
+
+		switch (Input.GetTouch(0).phase) {
+		case TouchPhase.Began:
+			tapBeganPos = Input.GetTouch (0).position;
+			break;
+		case TouchPhase.Moved:
+		case TouchPhase.Stationary:
+			touchTime += Time.deltaTime;
+
+			Vector3 vp = Input.GetTouch (0).deltaPosition;
+			if (vp.sqrMagnitude > tapDetectLengthSqrt) {
+				if (vp.x*vp.x > vp.y*vp.y){
+					OnScroll(Input.GetTouch(0).position.x, 0);
+				} else {
+					OnScroll(0, Input.GetTouch(0).position.y);
+				}
+			}
+			break;
+
+		case TouchPhase.Ended:
+		case TouchPhase.Canceled:
+
+			var endPos = Input.GetTouch (0).position;
+
+			//check tap and double tap.
+			if (touchTime < tapDetectTime) {
+				if ((endPos - tapBeganPos).sqrMagnitude < tapDetectLengthSqrt) {
+					if ((Time.time - lastTapTime) < doubleTapDetectTime) {
+						OnDoubleTap ();
+						lastTapTime = 0f;
+					} else {
+						OnTap ();
+						lastTapTime = Time.time;
+					}
+					touchTime = 0.0f;
+					return;
+				}
+			}
+
+			if (Mathf.Abs (endPos.x - tapBeganPos.x) > minSwipeDistX) {
+
+				if (endPos.x > tapBeganPos.x) {
+					OnSwipe ("RIGHTSwipe");
+				} else {
+					OnSwipe ("LEFTSwipe");
+				}
+			}else if (Mathf.Abs (endPos.y - tapBeganPos.y) > minSwipeDistY) {
+
+				if (endPos.y > tapBeganPos.y) {
+					OnSwipe ("UPSwipe");
+				} else{
+					OnSwipe ("DownSwipe");
+				}
+			}
+			touchTime = 0.0f;
+			break;
+		}
+
 	}
 }
